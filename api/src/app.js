52b08e875b02619d0d37a6c69b9839c2457b8a25
Samuel Aguiar <muka_gt@hotmail.com>
2021-05-12 21:38:40 -0300

    Criaçao do servidor node

---
+++ api/src/app.js
@@ -0,0 +1,44 @@
+import 'dotenv/config';
+
+import express from 'express';
+import path from 'path';
+import 'express-async-errors';
+import routes from './routes';
+
+import './database';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middleware();
+    this.routes();
+    this.exceptionHanler();
+  }
+
+  middleware() {
+    this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+
+  exceptionHanler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
+    });
+  }
+}
+
+export default new App().server;
